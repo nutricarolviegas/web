1fb14135c8f7d90571bdc44266ee9bc5b37da7e8
albberrrt <albberrrt.yayay@gmail.com>
2024-01-24 15:19:20 -0300

    fiex

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,3 @@
-import Animated from "./animated/index.tsx"
+import Animated from "./animated/index"
 
 export { Animated }
--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,4 +1,4 @@
-import Header from "./header/component.tsx";
+import Header from "./header/component";
 import Footer from "./footer/component";
 import RecipesSwiper from "./recipes-swiper/component";
 import SobreMim from "./sobre-mim/component";
